74bcf9c756f9649ee465d4a68c27ed30eba5f80e
phat nguyen <phatnguyen1901@gmail.com>
2020-09-18 22:38:42 -0500

    feat: add a text file that stores all 50 US states and add the Customer class

--- mockData.py
+++ mockData.py
@@ -1,4 +1,6 @@
 from faker import Faker
+import json
+import numpy as np
 
 fake_data = Faker()
 
@@ -12,9 +14,29 @@ class Customer:
         self.numberOfCars = numberOfCars
         self.householdIncome = householdIncome
 
+    # special method __repr__() to display data associated with object
+    def __repr__(self):
+        return ('''
+            age: {}, 
+            state: {}, 
+            number of kids: {}, 
+            number of cars: {}, 
+            household income: {}'''
+        .format(self.age, self.state, self.numberOfKids, self.numberOfCars, self.householdIncome))
+
+
+
+f = open('listOf50States.txt', 'r')
+# Remove all whitespace characters and split the string into a list of US states
+states = f.read().replace(' ', '').split(',')
 
 
 for _ in range(0,20):
-    profile = fake_data.simple_profile()
-    for k, v in profile.items():
-        print('{}: {}'.format(k, v))
+    customer = Customer(
+            np.random.randint(18,90, size=1, dtype=int)[0],\
+            fake_data.random_element(elements=states),\
+            fake_data.random_digit(),\
+            fake_data.random_digit(),\
+            np.random.randint(25000, 200000, size=1, dtype=int)[0]
+            )
+    print(customer)
