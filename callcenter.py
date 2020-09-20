9a7c2bb9dd0fca130a4318611ad524be7fb652c1
phat nguyen <phatnguyen1901@gmail.com>
2020-09-20 15:43:35 -0500

    feat: refactor the codebase, clean up and group related functions into the call center class

--- callcenter.py
+++ callcenter.py
@@ -1,90 +1,99 @@
-from mockData import create_customer, Agent
+from mockData import Agent, Customer
 import time
 import numpy as np
+from faker import Faker
+from functools import reduce
 
 
-customers = create_customer()
+class CallCenter:
 
-f = open('listOf50States.txt', 'r')
-states = f.read().replace(' ','').split(',')
+    def __init__ (self, noOfCustomers, noOfAgents):
+        self.noOfCustomers = noOfCustomers
+        self.noOfAgents = noOfAgents
+        self.fake_data = Faker()
+    
+    @staticmethod
+    def __match(customer, agents):
 
-agents = [
-    {
-        'id': 1,
-        'age': [18,30],
-        'state': states[:10],
-        'timestamp': 0
-    },
-    {
-        'id': 2,
-        'age': [31,50],
-        'state': states[11:30],
-        'timestamp': 0
-    },
-    {
-        'id': 3,
-        'age': [31,50],
-        'state': states[11:30],
-        'timestamp': 0
-    },
-    {
-        'id': 4,
-        'age': [51,70],
-        'state': states[31:40],
-        'timestamp': 0
-    },
-    {
-        'id': 5,
-        'age': [51,70],
-        'state': states[31:40],
-        'timestamp': 0
-    },
-    {
-        'id': 6,
-        'age': [71,90],
-        'state': states[41:50],
-        'timestamp': 0
-    }
-]
-
-def match(customer, agents):
-
-    return list(filter(lambda agent: agent['age'][0] <= customer.age and agent['age'][1] >= customer.age, agents))
+        return list(filter(lambda agent: agent.age[0] <= customer.age and agent.age[1] >= customer.age, agents))
 
 
-def call_center(customers, agents):
+    def __createCustomer(self):
 
-    call_back_customers = []
+        f = open('listOf50States.txt', 'r')
+        # Remove all whitespace characters and split the string into a list of US states
+        states = f.read().replace(' ', '').split(',')
+        housingStatus = ('rent', 'own')
 
-    for _ in range(19):
-        current_customer = next(customers)
-        matching_agents = match(current_customer, agents)
-        #print(matching_agents)
-        
-        if len(matching_agents) > 1: # if there more than 1 matching agent, randomly select one
-            random_number = np.random.randint(low=0, high=len(matching_agents)-1, size=1,dtype=int)[0]
+        for _ in range(0,self.noOfCustomers):
+            customer = Customer(
+                    np.random.randint(18,90, size=1, dtype=int)[0],\
+                    self.fake_data.random_element(elements=states),\
+                    self.fake_data.phone_number(),\
+                    self.fake_data.random_digit(),\
+                    self.fake_data.random_digit(),\
+                    self.fake_data.random_element(elements=housingStatus),\
+                    np.random.randint(25000, 200000, size=1, dtype=int)[0]
+                    )
             
-            random_timeout = np.random.randint(low=50, high=300, size=1, dtype=int)[0]
-            print(random_timeout)
-            selected_agent = matching_agents[random_number]
-            # if the current timestamp of the agent is less than timestamp now in epoch format, the agent 
-            if selected_agent['timestamp'] < int(round(time.time() * 1000)) and selected_agent['timestamp'] > 0:
-                print('agent is busy')
-                call_back_customers.append(current_customer)
-            else:
-            # set the timeout of this agent
-                selected_agent['timestamp'] = int(round(time.time() * 1000)) + random_timeout
+            yield customer
 
-    print(call_back_customers)
+    def __createAgent(self):
+
+        f = open('listOf50States.txt', 'r')
+        # Remove all whitespace characters and split the string into a list of US states
+        states = f.read().replace(' ', '').split(',')
+        housingStatus = ('rent', 'own')
+
+        for _ in range(0,self.noOfAgents):
+            agent = Agent(
+                    sorted(np.random.randint(18,90, size=2, dtype=int)),\
+                    self.fake_data.random_elements(elements=states),\
+                    self.fake_data.random_element(elements=housingStatus),\
+                    sorted(np.random.randint(25000, 200000, size=2, dtype=int))
+                    )
+            
+            yield agent
 
 
-call_center(customers, agents)
+    def createSimulation(self):
+        call_back_customers = []
+
+        agents = list(self.__createAgent())
+
+        for _ in range(self.noOfCustomers):
+            customers = self.__createCustomer()
+            current_customer = next(customers)
+            matching_agents = self.__match(current_customer, agents)
+            #print(matching_agents)
+            
+            if len(matching_agents) > 1: # if there more than 1 matching agent, randomly select one
+                random_number = np.random.randint(low=0, high=len(matching_agents)-1, size=1,dtype=int)[0]
+                
+                random_timeout = np.random.randint(low=50, high=300, size=1, dtype=int)[0]
+
+                selected_agent = matching_agents[random_number]
+                # if the current timestamp of the agent is less than timestamp now in epoch format, the agent 
+                if selected_agent.timestamp < int(round(time.time() * 1000)) and selected_agent.timestamp > 0:
+                    print('agent is busy')
+                    call_back_customers.append(current_customer)
+                else:
+                # set the timeout of this agent
+                    selected_agent.timestamp = int(round(time.time() * 1000)) + random_timeout
+
+        print(call_back_customers)
+        # calculate agent utilization
+        agentUtilized = len(list(filter(lambda agent: agent.timestamp != 0, agents)))
+        agentUtilization = agentUtilized/len(agents) * 100
+        print('Agent Utilization: %s %%' %agentUtilization)
 
 
-# Get a list of agents filtered by the attributes
-# available_list = filter(lambda agent: agents['state'] == , agents)
+noOfCustomers = 1000
+noOfAgents = 20
 
-# The function needs to 
+newSimulation = CallCenter(noOfCustomers, noOfAgents)
+newSimulation.createSimulation()
+
 
 '''
     filter based on two criteria
